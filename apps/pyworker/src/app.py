aa33c2837889c02d6805045b5518f3790b6c4247
Galdin Raphael <gldraphael@users.noreply.github.com>
2025-03-11 15:10:56 +0000

    Swap google's library with betterproto v2

--- apps/pyworker/src/app.py
+++ apps/pyworker/src/app.py
@@ -1,22 +1,17 @@
-from concurrent import futures
-import logging
-
-import grpc
-
-from scale.gen import scale_service_pb2_grpc
-from scale.services import ScaleService
+import asyncio
+from scale.gen.dev.galdin.scale import ClassifyObesityLevelRequest, ClassifyObesityLevelResponse, ScaleServiceBase, ObesityLevel
+from grpclib.server import Server
 
 
-def serve():
-    port = "50051"
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    scale_service_pb2_grpc.add_ScaleServiceServicer_to_server(ScaleService.ScaleService(), server)
-    server.add_insecure_port(f"[::]: {port}")
-    server.start()
-    print(f"Server started. Listening on port {port}")
-    server.wait_for_termination()
+class ScaleService(ScaleServiceBase):
+    async def classify_obesity_level(self, request: ClassifyObesityLevelRequest) -> ClassifyObesityLevelResponse:
+        return ObesityLevel.NORMAL_WEIGHT
 
+async def main():
+    server = Server([ScaleService()])
+    await server.start("127.0.0.1", 50051)
+    await server.wait_closed()
 
-if __name__ == "__main__":
-    logging.basicConfig()
-    serve()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
