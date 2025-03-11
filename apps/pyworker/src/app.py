662948fc0990f1df4ea91af2ba2152b81bc8f195
Galdin Raphael <gldraphael@users.noreply.github.com>
2025-03-11 19:47:44 +0000

    Update protobuf messages
    And pass the values around correctly

--- apps/pyworker/src/app.py
+++ apps/pyworker/src/app.py
@@ -1,11 +1,14 @@
 import asyncio
-from scale.gen.dev.galdin.scale import ClassifyObesityLevelRequest, ClassifyObesityLevelResponse, ScaleServiceBase, ObesityLevel
+from scale.gen.dev.galdin.scale import ClassifyObesityLevelRequest, ClassifyObesityLevelResponse, ScaleServiceBase, ObesityLevel, Sex
 from grpclib.server import Server
 
 
 class ScaleService(ScaleServiceBase):
     async def classify_obesity_level(self, request: ClassifyObesityLevelRequest) -> ClassifyObesityLevelResponse:
-        return ClassifyObesityLevelResponse(level=ObesityLevel.NORMAL_WEIGHT)
+        prediction = ObesityLevel.NORMAL_WEIGHT
+        if request.sex == Sex.MALE:
+            prediction = ObesityLevel.OVERWEIGHT_LEVEL_1
+        return ClassifyObesityLevelResponse(level=prediction)
 
 async def main():
     server = Server([ScaleService()])
