7b7578c8b94d35246c5d981a2fe9bfe7d5c19e99
Galdin Raphael <gldraphael@users.noreply.github.com>
2025-03-13 17:18:08 +0000

    Get a basic layout that looks reasonably ok

--- apps/reactapp/src/main.tsx
+++ apps/reactapp/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "@radix-ui/themes/styles.css";
+import { Container, Theme } from '@radix-ui/themes';
+import { ThemeProvider } from 'next-themes';
+import Layout from './Layout.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider attribute="class">
+      <Theme accentColor="purple" grayColor="mauve" radius="full" scaling="110%">
+        <Container size="1">
+            <Layout />
+        </Container>
+      </Theme>
+    </ThemeProvider>
   </StrictMode>,
 )
